24e6ae1fb91165bb52e1ca2a8b6aa09fd60bb7af
permania <0onesixed@gmail.com>
2026-02-05 08:34:31 -0800

    organize: reformat fish config

--- shell.nix
+++ shell.nix
@@ -1,42 +1,45 @@
-{pkgs, ...}: {
-  programs.fish = {
-    enable = true;
-    shellAliases = {};
-    functions = {
-      "fish_greeting".body = "";
-      "ls".body = "eza --long --group-directories-last --all --icons --no-time $argv";
+{ ... }:
+{
+  programs = {
+    fish = {
+      enable = true;
+      shellAliases = { };
+      functions = {
+        "fish_greeting".body = "";
+        "ls".body = "eza --long --group-directories-last --all --icons --no-time $argv";
+      };
     };
-  };
 
-  programs.zoxide = {
-    enable = true;
-    enableFishIntegration = true;
-  };
-  programs.eza = {
-    enable = true;
-    git = true;
-    enableFishIntegration = false;
-  };
-  programs.carapace = {
-    enable = true;
-    enableFishIntegration = true;
-  };
-  programs.nix-your-shell = {
-    enable = true;
-    enableFishIntegration = true;
-  };
-  programs.atuin = {
-    enable = true;
-    enableFishIntegration = true;
-  };
-  programs.pay-respects = {
-    enable = true;
-    enableFishIntegration = true;
-  };
-  programs.direnv = {
-    enable = true;
-    enableFishIntegration = true;
-    silent = true;
-    nix-direnv.enable = true;
+    zoxide = {
+      enable = true;
+      enableFishIntegration = true;
+    };
+    eza = {
+      enable = true;
+      git = true;
+      enableFishIntegration = false;
+    };
+    carapace = {
+      enable = true;
+      enableFishIntegration = true;
+    };
+    nix-your-shell = {
+      enable = true;
+      enableFishIntegration = true;
+    };
+    atuin = {
+      enable = true;
+      enableFishIntegration = true;
+    };
+    pay-respects = {
+      enable = true;
+      enableFishIntegration = true;
+    };
+    direnv = {
+      enable = true;
+      enableFishIntegration = true;
+      silent = true;
+      nix-direnv.enable = true;
+    };
   };
 }
